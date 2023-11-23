48eac8c35b2e73b472ff277cdf16e01ec19a196a
codehz <git@hertz.moe>
2023-11-23 22:57:25 +0800

    add dist script

---
+++ scripts/dist.ts
@@ -0,0 +1,36 @@
+import { spawnSync } from "node:child_process";
+import { cp, readFile, rm } from "node:fs/promises";
+
+await rm("dist", { recursive: true });
+
+await Bun.build({
+  target: "bun",
+  outdir: "dist",
+  minify: true,
+  splitting: false,
+  sourcemap: "inline",
+  external: [
+    "murmurhash-js",
+    "known-css-properties",
+    "fast-json-stable-stringify",
+    "csstype",
+  ],
+  entrypoints: ["build.ts", "index.js"],
+});
+
+for (const file of ["index.d.ts", "style.d.ts", "LICENSE", "README.md"]) {
+  await cp(file, `dist/${file}`);
+}
+
+const contents = JSON.parse(await readFile("package.json", "utf-8"));
+
+delete contents.private;
+delete contents.scripts;
+delete contents.devDependencies;
+contents.version = spawnSync("git", ["describe", "--tags"], {
+  encoding: "utf-8",
+})
+  .stdout.trim()
+  .replace(/-[[:digit:]]\+-g/, "+")
+  .replace(/^v/, "");
+await Bun.write("dist/package.json", JSON.stringify(contents, null, 2));
